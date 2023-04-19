df1c235feddcd4372e2468baa124f11d018b76a1
Saksham Mittal <gotlouemail@gmail.com>
2023-04-19 17:13:13 +0530

    Fix encrypted drive label

--- installnix.sh
+++ installnix.sh
@@ -42,16 +42,16 @@ btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
 umount /mnt
 
 # Mount the subvolumes with appropriate options
-mount -o subvol=root,compress=zstd,noatime /dev/mapper/enc /mnt
+mount -o subvol=root,compress=zstd,noatime /dev/mapper/cryptroot/mnt
 
 mkdir /mnt/home
-mount -o subvol=home,compress=zstd,noatime /dev/mapper/enc /mnt/home
+mount -o subvol=home,compress=zstd,noatime /dev/mapper/cryptroot/mnt/home
 
 mkdir /mnt/nix
-mount -o subvol=nix,compress=zstd,noatime /dev/mapper/enc /mnt/nix
+mount -o subvol=nix,compress=zstd,noatime /dev/mapper/cryptroot/mnt/nix
 
 mkdir /mnt/persist
-mount -o subvol=persist,compress=zstd,noatime /dev/mapper/enc /mnt/persist
+mount -o subvol=persist,compress=zstd,noatime /dev/mapper/cryptroot/mnt/persist
 
 # don't forget this!
 mkdir /mnt/boot
