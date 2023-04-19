d98d7f9fde203ab3b77749e0cfc5c447dc3d126c
Saksham Mittal <gotlouemail@gmail.com>
2023-04-19 23:29:49 +0530

    Remove unnecessary fields from filesystem desc

--- configuration.nix
+++ configuration.nix
@@ -18,21 +18,18 @@
 
   fileSystems."/home" =
     {
-      device = "/dev/disk/by-label/root";
       options = [ "subvol=home" "compress=zstd" "noatime" ];
       fsType = "btrfs";
     };
 
   fileSystems."/nix" =
     {
-      device = "/dev/disk/by-label/root";
       options = [ "subvol=nix" "compress=zstd" "noatime" ];
       fsType = "btrfs";
     };
 
   fileSystems."/persist" =
     {
-      device = "/dev/disk/by-label/root";
       options = [ "subvol=persist" "compress=zstd" "noatime" ];
       fsType = "btrfs";
     };
