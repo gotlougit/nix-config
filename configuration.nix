10c1d500f63ef30e2ba3c57d4690add803f22ba0
Saksham Mittal <gotlouemail@gmail.com>
2023-04-20 09:00:29 +0530

    Enable KDE Plasma install

--- configuration.nix
+++ configuration.nix
@@ -99,8 +99,8 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
