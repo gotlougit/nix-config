5d131c0efe23983c7d3fb82d4d4b9e2666336244
Saksham Mittal <gotlouemail@gmail.com>
2023-04-21 11:36:34 +0530

    Update enabling flake command

--- addconfig.sh
+++ addconfig.sh
@@ -5,19 +5,19 @@
 # It is a separate script right now in order to better test the script and decouple it from having to run the partitioning steps again.
 
 # Check for presence of required files
-if [! -f "flake.lock"]; then
+if [ ! -f "flake.lock" ]; then
     echo "Did not find flake.lock"
     echo "Hint: did you git clone the repo?"
     exit
 fi
 
-if [! -f "flake.nix"]; then
+if [ ! -f "flake.nix" ]; then
     echo "Did not find flake.nix"
     echo "Hint: did you git clone the repo?"
     exit
 fi
 
-if [! -f "configuration.nix"]; then
+if [ ! -f "configuration.nix" ]; then
     echo "Did not find configuration.nix"
     echo "Hint: did you git clone the repo?"
     exit
@@ -31,8 +31,7 @@ cp flake.lock /mnt/etc/nixos/flake.lock
 echo "Copied configuration to right path"
 
 # Enable nix flake
-echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
-nix flake update
+nix flake update --extra-experimental-features 'nix-command flakes'
 
 echo "Please run 'sudo nixos-install' to start the installer"
 echo "ALSO: TAKE THIS TIME TO EDIT CONFIG to better suit your purposes/hardware"
