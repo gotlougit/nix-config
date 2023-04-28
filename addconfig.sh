133abf7fa5d5b7608812fdbc5d3a26ca5882ed67
Saksham Mittal <gotlouemail@gmail.com>
2023-04-28 23:18:53 +0530

    Use addconfig.sh to automate copying custom config

--- addconfig.sh
+++ addconfig.sh
@@ -1,39 +1,12 @@
 #!/usr/bin/env bash
 
-# This script automates copying config to the right path and generating the config based on nix flake
+# This script automates copying config to the right path and generating the system
 
-# It is a separate script right now in order to better test the script and decouple it from having to run the partitioning steps again.
+echo "Downloading config..."
+curl -O https://raw.githubusercontent.com/gotlougit/nix-config/main/configuration.nix
 
-# Check for presence of required files
-if [ ! -f "flake.lock" ]; then
-    echo "Did not find flake.lock"
-    echo "Hint: did you git clone the repo?"
-    exit
-fi
+cp configuration.nix /etc/nixos/configuration.nix
+echo "Copied config to /etc/nixos"
 
-if [ ! -f "flake.nix" ]; then
-    echo "Did not find flake.nix"
-    echo "Hint: did you git clone the repo?"
-    exit
-fi
-
-if [ ! -f "configuration.nix" ]; then
-    echo "Did not find configuration.nix"
-    echo "Hint: did you git clone the repo?"
-    exit
-fi
-
-# Copy the required files to new system
-echo "Copying config to the mounted system..."
-cp configuration.nix /mnt/etc/nixos/configuration.nix
-cp flake.nix /mnt/etc/nixos/flake.nix
-cp flake.lock /mnt/etc/nixos/flake.lock
-echo "Copied configuration to right path"
-
-# Enable nix flake
-cd /mnt/etc/nixos/
-nix flake update --extra-experimental-features 'nix-command flakes'
-
-echo "Please run 'sudo nixos-install' to start the installer"
-echo "ALSO: TAKE THIS TIME TO EDIT CONFIG to better suit your purposes/hardware"
-echo "When you are done, you can run this script again to initiate the build step again"
+echo "Running rebuild command..."
+nixos-rebuild switch
