f9e60db56f66f0d5f82179f2695c915385cecbdc
Saksham Mittal <gotlouemail@gmail.com>
2023-06-04 11:54:15 +0530

    Update bootstrap.sh with flake based commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,14 @@
 # This script automates copying config to the right path and generating the system
 
 echo "Downloading config..."
+mkdir nixos-config
+cd nixos-config
 curl -O https://raw.githubusercontent.com/gotlougit/nix-config/main/configuration.nix
-
-cp configuration.nix /etc/nixos/configuration.nix
+curl -O https://raw.githubusercontent.com/gotlougit/nix-config/main/hardware-configuration.nix
+curl -O https://raw.githubusercontent.com/gotlougit/nix-config/main/flake.nix
+curl -O https://raw.githubusercontent.com/gotlougit/nix-config/main/flake.lock
+cp *.nix flake.lock /etc/nixos/
 echo "Copied config to /etc/nixos"
 
 echo "Running rebuild command..."
-nixos-rebuild switch
+nixos-rebuild switch --flake .#kratos
