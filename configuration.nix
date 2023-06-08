7e67c75eadab59bfa1245950aee1683458601bf9
Saksham Mittal <gotlouemail@gmail.com>
2023-06-08 12:07:09 +0530

    Remove mullvad-browser sandbox alias and overlay

--- configuration.nix
+++ configuration.nix
@@ -328,7 +328,7 @@
     switch-config = "sudo nixos-rebuild switch --flake /home/gotlou/Code/nixos#kratos";
     update-config = "sudo nix flake update /home/gotlou/Code/nixos && sudo nixos-rebuild switch --flake /home/gotlou/Code/nixos#kratos";
     switch-config-boot = "sudo nixos-rebuild boot --flake /home/gotlou/Code/nixos#kratos";
-    mullvad-browser = "mullvad-browser-sandbox";
+    #mullvad-browser = "mullvad-browser-sandbox";
   };
   # Hook direnv
   programs.bash.interactiveShellInit = ''
--- mullvad-overlay.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-let
-  mullvad-browser = super.mullvad-browser;
-in
-super // {
-  mullvad-browser = super.mullvad-browser-sandbox;
-}
