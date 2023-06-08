78f3a8be3c95ad710a9ab3dc2cf950f8eb1b8532
Saksham Mittal <gotlouemail@gmail.com>
2023-06-08 11:22:27 +0530

    Remove dependencies of sandbox

--- configuration.nix
+++ configuration.nix
@@ -259,7 +259,6 @@
     aspell # CLI spell check
     bandwhich # view what programs are making network requests
     bear # Generate autocomplete for large projects
-    bubblewrap # Sandboxing utility
     compsize # View btrfs compression info
     curl # no intro needed
     inputs.code-sandbox.packages.x86_64-linux.default # Installs sandboxes
@@ -291,7 +290,6 @@
     postgresql_15 # Database
     rr # Record and replay while debugging
     radeontop # AMD GPU monitoring
-    slirp4netns
     syncthing # Sync all files
     tailscale # Create a mesh network
     trash-cli # Send file to trash instead of permanently deleting
