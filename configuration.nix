888b9838310be49eb00d4194886d986a2d3e8b57
Saksham Mittal <gotlouemail@gmail.com>
2023-06-09 16:10:46 +0530

    Remove old config option

--- configuration.nix
+++ configuration.nix
@@ -124,8 +124,6 @@
     dockerCompat = true;
 
     # Required for containers under podman-compose to be able to talk to each other.
-    defaultNetwork.dnsname.enable = true;
-    # For Nixos version > 22.11
     defaultNetwork.settings = {
       dns_enabled = true;
     };
