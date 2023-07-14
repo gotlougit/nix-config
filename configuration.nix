b234334215e5541353ce16f54f6f890f01108c03
Saksham Mittal <gotlouemail@gmail.com>
2023-07-14 16:50:05 +0530

    Add wezterm

--- configuration.nix
+++ configuration.nix
@@ -249,6 +249,7 @@
       tdesktop # Telegram desktop client
       tor-browser-bundle-bin # Needs no intro
       vlc # Easiest media player
+      wezterm # GPU accelerated terminal emulator written in Rust
       wl-clipboard # CLI util for copying and pasting in Wayland
 
       # Development Stuff
