34cbdca751d9f856aed2278ce7a9372c945d027e
Saksham Mittal <gotlouemail@gmail.com>
2023-07-25 12:45:02 +0530

    Change shell commands to change depending upon hostname

--- kratos/shell.nix
+++ kratos/shell.nix
@@ -1,12 +1,12 @@
-{ ... }:
+{ config, ... }:
 {
   # Set useful shell aliases
   programs.bash.shellAliases = {
     vi = "hx";
     open = "xdg-open";
-    switch-config = "sudo nixos-rebuild switch --flake /home/gotlou/nixos#kratos";
-    update-config = "sudo nix flake update /home/gotlou/nixos && sudo nixos-rebuild switch --flake /home/gotlou/nixos#kratos";
-    switch-config-boot = "sudo nixos-rebuild boot --flake /home/gotlou/nixos#kratos";
+    switch-config = "sudo nixos-rebuild switch --flake /home/gotlou/nixos#${config.networking.hostName}";
+    update-config = "sudo nix flake update /home/gotlou/nixos && sudo nixos-rebuild switch --flake /home/gotlou/nixos#${config.networking.hostName}";
+    switch-config-boot = "sudo nixos-rebuild boot --flake /home/gotlou/nixos#${config.networking.hostName}";
     mullvad-browser = "mullvad-browser-sandbox";
     cat = "bat";
     diff = "difft";
