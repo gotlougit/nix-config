b84f0bd12fa5cf31f30f1fb633ef89ec11faca4d
Saksham Mittal <gotlouemail@gmail.com>
2023-07-25 12:33:13 +0530

    Remove unnecessary steam enable statement

--- kratos/configuration.nix
+++ kratos/configuration.nix
@@ -99,8 +99,6 @@
   # Enable library and dev documentation
   documentation.dev.enable = true;
   
-  programs.steam.enable = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
