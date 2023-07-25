3481316d0248f375f8ce68a5e33b8aeb13b87af0
Saksham Mittal <gotlouemail@gmail.com>
2023-07-25 12:48:53 +0530

    Change hostname to mimir

--- mimir/configuration.nix
+++ mimir/configuration.nix
@@ -3,7 +3,7 @@
   time.timeZone = "Asia/Kolkata";
   services.openssh.enable = true;
   services.tailscale.enable = true;
-  networking.hostName = "pi";
+  networking.hostName = "mimir";
   users = {
     users.gotlou = {
       password = "myPassword";
