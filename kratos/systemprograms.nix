548e1d92f7aa34bdff6c137a759e8fcb88420c15
Saksham Mittal <gotlouemail@gmail.com>
2023-08-23 21:42:36 +0530

    rm: remove rustscan

--- kratos/systemprograms.nix
+++ kratos/systemprograms.nix
@@ -54,7 +54,6 @@
     pinentry # Enter gpg password securely
     postgresql_15 # Database
     rr # Record and replay while debugging
-    rustscan # Faster replacement for nmap
     radeontop # AMD GPU monitoring
     syncthing # Sync all files
     snowflake # Pluggable transport for tor
