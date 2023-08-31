8abae55b72c4d179438b517445ac5db2b8d81aaf
Saksham Mittal <gotlouemail@gmail.com>
2023-08-31 23:00:01 +0530

    feat: enable MAC randomization

--- kratos/configuration.nix
+++ kratos/configuration.nix
@@ -27,6 +27,9 @@
   networking.hostName = "kratos";
   # Enable networkmanager
   networking.networkmanager.enable = true;
+  # Randomize MAC address
+  networking.networkmanager.wifi.macAddress = "stable";
+  networking.networkmanager.ethernet.macAddress = "stable";
   # Allow KDE connect
   networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
   networking.firewall.allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
