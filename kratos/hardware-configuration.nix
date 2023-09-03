b942c2b68bde36e069ca8a323efe96013d486912
Saksham Mittal <gotlouemail@gmail.com>
2023-09-03 10:43:38 +0530

    fix: change governor to powersave

--- kratos/hardware-configuration.nix
+++ kratos/hardware-configuration.nix
@@ -63,5 +63,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  powerManagement.cpuFreqGovernor = "schedutil";
+  powerManagement.cpuFreqGovernor = "powersave";
 }
