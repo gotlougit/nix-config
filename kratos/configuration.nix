662dd211c677addc0e9eadd17199835aced6962a
Saksham Mittal <gotlouemail@gmail.com>
2023-09-05 23:17:29 +0530

    feat: open up port for Syncthing

--- kratos/configuration.nix
+++ kratos/configuration.nix
@@ -33,6 +33,8 @@
   # Allow KDE connect
   networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
   networking.firewall.allowedUDPPortRanges = [{ from = 1714; to = 1764; }];
+  # Allow Syncthing
+  networking.firewall.allowedUDPPorts = [ 22000 ];
 
   # Use the systemd-boot bootloader.
   boot.loader.systemd-boot.enable = true;
