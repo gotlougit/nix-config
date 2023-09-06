412f38fa7690c58c5c228423a157808aed605e83
Saksham Mittal <gotlouemail@gmail.com>
2023-09-06 19:34:36 +0530

    feat: add more configs into git

--- home/git.nix
+++ home/git.nix
@@ -10,6 +10,13 @@
       gpg.format = "ssh";
       user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdqk3Yo5e67SNiTiZlIMlDHI5cD9L3hFatrFo+yM7zB signing-key";
       commit.gpgsign = true;
+      init.defaultBranch = "main";
+      diff.renames = "copy";
+      core.autocrlf = "input";
+      core.autoclrf = false;
+      core.filemode = false;
+      push.default = "current";
+      gpg.ssh.allowedSignersFile = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdqk3Yo5e67SNiTiZlIMlDHI5cD9L3hFatrFo+yM7zB signing-key";
     };
     # Enable syntax highlight in diffs
     delta.enable = true;
