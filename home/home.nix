7bc362775d508e38aa49aa9b0668517649139c2e
Saksham Mittal <gotlouemail@gmail.com>
2023-09-06 23:11:14 +0530

    home-manager: add scli config

--- home/home.nix
+++ home/home.nix
@@ -4,6 +4,7 @@
     ./git.nix
     ./helix.nix
     ./plasma.nix
+    ./scli.nix
     ./wezterm.nix
   ];
   home.stateVersion = "22.11";
---
+++ home/scli.nix
@@ -0,0 +1,10 @@
+{ ... }:
+
+{
+  xdg.configFile."sclirc".text = ''
+    wrap-at = 80
+    save-history = true
+    partition-contacts = true
+    show-names = true
+  '';
+}
