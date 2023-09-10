e64654838ed2769aa52e4b467d11d1696245f236
Saksham Mittal <gotlouemail@gmail.com>
2023-09-11 00:36:53 +0530

    feat: use chrony and enable NTS

--- flake.nix
+++ flake.nix
@@ -73,6 +73,7 @@
             ./hosts/kratos/services/tailscale.nix
             ./hosts/shared/services/bluetooth.nix
             # ./hosts/shared/services/opensnitch.nix
+            ./hosts/shared/services/time.nix
             ./hosts/shared/services/warp.nix
             ./hosts/shared/services/waydroid.nix
             ./hosts/shared/services/virt.nix
---
+++ hosts/shared/services/time.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  # Just to be safe, we disable default systemd-timesyncd
+  services.timesyncd.enable = false;
+  services.chrony = {
+    enable = true;
+    enableNTS = true;
+    servers = [ "time.cloudflare.com" "nts.netnod.se" ];
+  };
+}
