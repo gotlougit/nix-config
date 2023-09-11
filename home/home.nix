af0811652cbcbcbb20c28a32d6d06474b4d5c746
Saksham Mittal <gotlouemail@gmail.com>
2023-09-11 18:13:42 +0530

    feat: add OTPClient to home-manager

--- home/home.nix
+++ home/home.nix
@@ -7,6 +7,7 @@
     ./htop.nix
     ./keepassxc.nix
     ./macchina.nix
+    ./otpclient.nix
     ./plasma/plasma.nix
     ./scli.nix
     ./wezterm.nix
---
+++ home/otpclient.nix
@@ -0,0 +1,10 @@
+{ lib, ... }:
+{
+  xdg.configFile."otpclient.cfg".text = lib.generators.toINI { } {
+    config = {
+      db_path = "/home/gotlou/Documents/DB/twofacauth";      
+      auto_lock=false;
+      inactivity_timeout=120;
+    };
+  };
+}
