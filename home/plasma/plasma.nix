80b12613be7fb62079ed815bd39f28bfa444aa0f
Saksham Mittal <gotlouemail@gmail.com>
2023-09-13 22:41:45 +0530

    home-manager: disable window border on fullscreening apps

--- home/plasma/plasma.nix
+++ home/plasma/plasma.nix
@@ -478,6 +478,7 @@
       "kwinrc"."Tiling.bd455426-91b5-5aa4-b78c-9e21c3c810ab"."tiles" = "{\"layoutDirection\":\"floating\",\"tiles\":[]}";
       "kwinrc"."Tiling.cc55641a-b969-52cd-87bf-88d4810033db"."tiles" = "{\"layoutDirection\":\"horizontal\",\"tiles\":[{\"width\":0.25},{\"width\":0.5},{\"width\":0.25}]}";
       "kwinrc"."Windows"."SnapOnlyWhenOverlapping" = true;
+      "kwinrc"."Windows"."BorderlessMaximizedWindows" = true;
       "kwinrc"."Xwayland"."Scale" = 1;
       "plasma-localerc"."Formats"."LANG" = "en_IN";
       "plasmanotifyrc"."Notifications"."PopupPosition" = "TopRight";
