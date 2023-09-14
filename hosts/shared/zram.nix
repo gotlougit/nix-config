ad76e3dd12e93763440cf674a22d6c920f674003
Saksham Mittal <gotlouemail@gmail.com>
2023-09-14 10:11:26 +0530

    fix: increase vm.swappiness

--- hosts/shared/zram.nix
+++ hosts/shared/zram.nix
@@ -5,7 +5,7 @@
   zramSwap.memoryPercent = 150;
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 180;
+    "vm.swappiness" = 200;
     "vm.watermark_boost_factor" = 0;
     "vm.watermark_scale_factor" = 125;
     "vm.page-cluster" = 0;
