0a7cc50a665cd9a7b8d1f07d032051758f3fe454
Saksham Mittal <gotlouemail@gmail.com>
2023-09-15 13:14:43 +0530

    home-manager: document git.nix

--- home/git.nix
+++ home/git.nix
@@ -3,20 +3,24 @@
 {
   programs.git = {
     enable = true;
+    # Set name and email to use when creating commits
     userName = "Saksham Mittal";
     userEmail = "gotlouemail@gmail.com";
-    # Signing config
     extraConfig = {
+      # Signing config
       gpg.format = "ssh";
       user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdqk3Yo5e67SNiTiZlIMlDHI5cD9L3hFatrFo+yM7zB signing-key";
+      gpg.ssh.allowedSignersFile = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdqk3Yo5e67SNiTiZlIMlDHI5cD9L3hFatrFo+yM7zB signing-key";
       commit.gpgsign = true;
+      # Make default branch be named "main"
       init.defaultBranch = "main";
       diff.renames = "copy";
+      # Some file ending stuff
       core.autocrlf = "input";
       core.autoclrf = false;
       core.filemode = false;
+      # Push current branch if nothing is specified
       push.default = "current";
-      gpg.ssh.allowedSignersFile = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdqk3Yo5e67SNiTiZlIMlDHI5cD9L3hFatrFo+yM7zB signing-key";
     };
     # Enable syntax highlight in diffs
     delta.enable = true;
