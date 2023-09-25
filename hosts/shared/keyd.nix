5d929a68a85852a2437f6664943534af3f4f613b
Saksham Mittal <gotlouemail@gmail.com>
2023-09-25 13:09:05 +0530

    feat: add InaccessiblePaths to keyd service

--- hosts/shared/keyd.nix
+++ hosts/shared/keyd.nix
@@ -56,6 +56,7 @@ in
       "@system-service"
       "~@privileged"
     ];
+    InaccessiblePaths = "/persist";
     RestrictAddressFamilies = [ "AF_UNIX" ];
     RestrictSUIDSGID = true;
     IPAddressDeny = [ "any" ];
