a2ec932952db17e641a2482593fccb4f926bff01
Saksham Mittal <git@gotlou.com>
2023-11-13 22:11:31 +0530

    fix: disable most earlyoom sandboxing for it to work

--- system/oom.nix
+++ system/oom.nix
@@ -10,28 +10,10 @@
   systemd.services.earlyoom = {
     enable = true;
     serviceConfig = {
-      NoNewPrivileges = true;
-      LockPersonality = true;
-      PrivateMounts = true;
-      PrivateTmp = true;
-      ProtectControlGroups = true;
-      ProtectHostname = true;
-      ProtectKernelLogs = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectSystem = true;
       PrivateNetwork = true;
-      RestrictNamespaces = true;
-      RestrictRealtime = true;
-      RestrictSUIDSGID = true;
       MemoryDenyWriteExecute = true;
       InaccessiblePaths = "/persist";
-      CapabilityBoundingSet = [ "" ];
       ProtectHome = true;
-      ProtectClock = true;
-      SystemCallArchitectures = "native";
-      SystemCallFilter = [ "@system-service" ];
-      UMask = "0077";
     };
   };
 }
