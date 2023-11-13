9ad2f3342da4d5a80840f527b2584f6e413958f5
Saksham Mittal <git@gotlou.com>
2023-11-13 21:05:41 +0530

    feat: harden vnstat

--- system/standard-services.nix
+++ system/standard-services.nix
@@ -3,6 +3,10 @@
 {
   # Enable vnstatd to monitor total net usage
   services.vnstat.enable = true;
+  systemd.services.vnstat = {
+    enable = true;
+    serviceConfig = (import ./hardening-base.nix);
+  };
   # Enable firmware updates
   services.fwupd.enable = true;
   # Enable AppArmor for more security
