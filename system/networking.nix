bf1e9419f660a9bd2a115bf6b206a99f05ba0e40
Saksham Mittal <git@gotlou.com>
2023-11-13 20:29:14 +0530

    fix: temporarily remove NetworkManager hardening

--- system/networking.nix
+++ system/networking.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   # Setup networking
   networking = {
@@ -19,10 +19,30 @@
     # if they are enabled, so we don't need to declare that here
     firewall.enable = true;
   };
-  systemd.services.NetworkManager = {
-    enable = true;
-    serviceConfig = (import ./hardening-base.nix);
-  };
+  # systemd.services.NetworkManager = {
+  #   enable = true;
+  #   serviceConfig = {
+  #     ProtectProc = false;
+  #     LockPersonality = true;
+  #     MemoryDenyWriteExecute = true;
+  #     NoNewPrivileges = true;
+  #     PrivateMounts = true;
+  #     PrivateTmp = true;
+  #     ProtectClock = true;
+  #     ProtectControlGroups = true;
+  #     ProtectHome = true;
+  #     ProtectHostname = true;
+  #     ProtectKernelLogs = true;
+  #     ProtectKernelModules = true;
+  #     ProtectKernelTunables = true;
+  #     RestrictRealtime = true;
+  #     RestrictSUIDSGID = true;
+  #     SystemCallArchitectures = "native";
+  #     # SystemCallFilter = [ "@known" "~@clock" "~@cpu-emulation" "~@raw-io" "~@reboot" "~@mount" "~@obsolete" "~@swap" "~@debug" "~@keyring" "~@pkey" "~@chown" ];
+  #     UMask = "0077";
+  #     InaccessiblePaths = "/persist";
+  #   };
+  # };
   systemd.services.NetworkManager-dispatcher = {
     enable = true;
     serviceConfig = (import ./hardening-base.nix);
