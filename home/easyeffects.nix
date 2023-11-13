407b6fc83cabc403d6d73c7b588b59ca26dd723f
Saksham Mittal <git@gotlou.com>
2023-11-13 22:53:42 +0530

    feat: add EasyEffects profile to repo
    This was taken from https://github.com/sebastian-de/easyeffects-thinkpad-unsuck
    and seems to sound decently better than without any DSP

--- home/easyeffects.nix
+++ home/easyeffects.nix
@@ -3,4 +3,5 @@
 {
   services.easyeffects.enable = true;
   services.easyeffects.preset = "thinkpad-unsuck";
+  xdg.configFile."easyeffects/output/thinkpad-unsuck.json".source = ./thinkpad-unsuck.json;
 }
