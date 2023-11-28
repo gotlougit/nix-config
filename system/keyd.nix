7a3878472f51cc6c51e3a1c44c51baabd360383c
Saksham Mittal <git@gotlou.com>
2023-11-28 16:54:33 +0530

    keyd: reinstate backspace

--- system/keyd.nix
+++ system/keyd.nix
@@ -18,7 +18,6 @@ let
       "rightcontrol" = "overload(control, ])";
       # FIXME when I don't need these to go cold turkey
       # This is a learning tool
-      "backspace" = "noop";
       "delete" = "noop";
       "S-9" = "noop";
       "S-0" = "noop";
