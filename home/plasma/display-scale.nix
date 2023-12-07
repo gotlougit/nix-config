6a46919109d1f1b95fe31dd54aedf99981f66500
Saksham Mittal <git@gotlou.com>
2023-12-07 16:39:15 +0530

    plasma: set monitor to 120Hz

--- home/plasma/display-scale.nix
+++ home/plasma/display-scale.nix
@@ -29,25 +29,25 @@
         }
     ]
   '';
-  home.file.".local/share/kscreen/outputs/84444ae82081584df261b8687437ac41".text = ''
+  home.file.".local/share/kscreen/outputs/b987fbbe8e6ff47c77fa4cc4f7b9fc06".text = ''
     {
-      "id": "84444ae82081584df261b8687437ac41",
-      "metadata": {
-          "fullname": "xrandr-unknown",
-          "name": "eDP-1"
-      },
-      "mode": {
-          "refresh": 60.04899978637695,
-          "size": {
-              "height": 1080,
-              "width": 1920
-          }
-      },
-      "overscan": 0,
-      "rgbrange": 0,
-      "rotation": 1,
-      "scale": 1,
-      "vrrpolicy": 0
+        "id": "b987fbbe8e6ff47c77fa4cc4f7b9fc06",
+        "metadata": {
+            "fullname": "xrandr-24G2W1G4-WWTU2P2000027",
+            "name": "HDMI-A-1"
+        },
+        "mode": {
+            "refresh": 120,
+            "size": {
+                "height": 1080,
+                "width": 1920
+            }
+        },
+        "overscan": 0,
+        "rgbrange": 0,
+        "rotation": 1,
+        "scale": 1,
+        "vrrpolicy": 0
     }
   '';
 }
