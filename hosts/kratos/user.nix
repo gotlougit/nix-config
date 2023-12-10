52ae8673d0c702e782c3bd993f6498d1672dbf03
Saksham Mittal <git@gotlou.com>
2023-12-10 16:37:38 +0530

    feat: use elisa instead of musikcube

--- hosts/kratos/user.nix
+++ hosts/kratos/user.nix
@@ -42,7 +42,7 @@
       mangohud # Overlay while playing games
       minetest # Open source Minecraft-like game
       mullvad-browser # Browser for private browsing
-      musikcube
+      elisa
       otpclient # TOTP client
       duckstation # PS1 emulator
       pcsx2 # PS2 emulator
