6e1b9f71a321a5daa894605a0edba599d8fdc40e
Saksham Mittal <git@gotlou.com>
2023-12-29 20:55:59 +0530

    user: set home dir

--- hosts/kratos/user.nix
+++ hosts/kratos/user.nix
@@ -6,6 +6,7 @@
   users.mutableUsers = false;
   users.users.gotlou = {
     isNormalUser = true;
+    home = "/home/gotlou";
     hashedPassword = "$y$j9T$rw1wMForHIg1XuK9HWnCD0$j.19g/PKjzKFPgEF/X2.lMOLIrXLfgGAQ9m9zz85Bc5";
     extraGroups = [ "wheel" "wireshark" "networkmanager" config.users.groups.keys.name "libvirtd" ];
     # TODO: make this even more comprehensive
