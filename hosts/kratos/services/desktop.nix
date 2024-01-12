e18b4677df553017b2c7345026ffedd7316bfd79
Saksham Mittal <git@gotlou.com>
2024-01-12 12:06:08 +0530

    desktop: remove X11 config

--- hosts/kratos/services/desktop.nix
+++ hosts/kratos/services/desktop.nix
@@ -3,17 +3,12 @@
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.autorun = true;
   # Enable use of KDE Plasma and SDDM login manager
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   # Set wayland as default
   services.xserver.displayManager.defaultSession = "plasmawayland";
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  # Disable touchpad while typing
-  services.xserver.libinput.touchpad.disableWhileTyping = true;
   # Extra SDDM config
   services.xserver.displayManager.sddm.settings = {
     Theme = {
@@ -54,9 +49,6 @@
       "Indexing-Enabled" = false;
     };
   };
-
-  # Configure keymap in X11
-  services.xserver.layout = "us";
 
   # Enable CUPS to print documents.
   services.printing.enable = false;
