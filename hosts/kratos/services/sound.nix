448b1ef9b705bffdc29e0e022fbf4452146698bc
Saksham Mittal <git@gotlou.com>
2024-03-08 17:03:35 +0530

    fix: remove old wireplumber config

--- hosts/kratos/services/sound.nix
+++ hosts/kratos/services/sound.nix
@@ -14,8 +14,8 @@
 
   # Some WirePlumber stuff to handle bluetooth correctly
   services.pipewire.wireplumber.enable = true;
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text =
-      "	bluez_monitor.properties = {\n		[\"bluez5.enable-sbc-xq\"] = true,\n		[\"bluez5.enable-msbc\"] = true,\n		[\"bluez5.enable-hw-volume\"] = true,\n		[\"bluez5.headset-roles\"] = \"[ hsp_hs hsp_ag hfp_hf hfp_ag ]\"\n	}\n";
-  };
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text =
+  #     "	bluez_monitor.properties = {\n		[\"bluez5.enable-sbc-xq\"] = true,\n		[\"bluez5.enable-msbc\"] = true,\n		[\"bluez5.enable-hw-volume\"] = true,\n		[\"bluez5.headset-roles\"] = \"[ hsp_hs hsp_ag hfp_hf hfp_ag ]\"\n	}\n";
+  # };
 }
