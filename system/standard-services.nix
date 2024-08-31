19a6cf2516821ec403863bc8bbc710b7ea97c1ac
Saksham Mittal <git@gotlou.com>
2024-08-31 13:27:28 +0530

    dbus-broker: remove

--- system/standard-services.nix
+++ system/standard-services.nix
@@ -9,9 +9,6 @@
   };
   # Enable firmware updates
   services.fwupd.enable = true;
-  # Use dbus-broker for better performane
-  services.dbus.enable =  true;
-  services.dbus.implementation = "broker";
   # Use irqbalance to help handle system being laggy during high load
   services.irqbalance.enable = true;
 }
