fb9bac8152f502ebd768bba4c5847897bf54725f
Saksham Mittal <git@gotlou.com>
2024-12-10 13:40:03 +0530

    cloudflare-warp-old: use slightly newer version

--- overlays/cloudflare-warp-old.nix
+++ overlays/cloudflare-warp-old.nix
@@ -1,35 +1,19 @@
-{ stdenv
-, lib
-, fetchurl
-, dpkg
-, autoPatchelfHook
-, makeWrapper
-, copyDesktopItems
-, makeDesktopItem
-, dbus
-, nftables
-}:
+{ stdenv, lib, autoPatchelfHook, copyDesktopItems, dbus, dpkg, fetchurl, gtk3
+, libpcap, makeDesktopItem, makeWrapper, nftables }:
 
 stdenv.mkDerivation rec {
   pname = "cloudflare-warp-old";
-  version = "2023.3.258";
+  version = "2024.4.133-1";
 
   src = fetchurl {
-    url = "https://web.archive.org/web/20230405213216if_/https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2023_3_258_1_amd64_f876b846af.deb";
-    hash = "sha256-Iw32HKMcxzO8mqHAbggZ5zP0qw+BN8fnopX+nS/UjDQ=";
+    url =
+      "https://web.archive.org/web/20240510075136if_/https://pkg.cloudflareclient.com/pool/focal/main/c/cloudflare-warp/cloudflare-warp_2024.4.133-1_amd64.deb";
+    hash = "sha256-Z2MgAAIty5pYH58wb1ZqwKdFGJSts6nt9/YHokNMgCs=";
   };
 
-  nativeBuildInputs = [
-    dpkg
-    autoPatchelfHook
-    makeWrapper
-    copyDesktopItems
-  ];
+  nativeBuildInputs = [ dpkg autoPatchelfHook makeWrapper copyDesktopItems ];
 
-  buildInputs = [
-    dbus
-    stdenv.cc.cc.lib
-  ];
+  buildInputs = [ dbus gtk3 libpcap stdenv.cc.cc.lib ];
 
   desktopItems = [
     (makeDesktopItem {
@@ -44,9 +28,7 @@ stdenv.mkDerivation rec {
     })
   ];
 
-  unpackPhase = ''
-    dpkg-deb -x ${src} ./
-  '';
+  autoPatchelfIgnoreMissingDeps = [ "libpcap.so.0.8" ];
 
   installPhase = ''
     runHook preInstall
@@ -54,6 +36,7 @@ stdenv.mkDerivation rec {
     mv usr $out
     mv bin $out
     mv etc $out
+    patchelf --replace-needed libpcap.so.0.8 ${libpcap}/lib/libpcap.so $out/bin/warp-dex
     mv lib/systemd/system $out/lib/systemd/
     substituteInPlace $out/lib/systemd/system/warp-svc.service \
       --replace "ExecStart=" "ExecStart=$out"
@@ -61,6 +44,7 @@ stdenv.mkDerivation rec {
       --replace "ExecStart=" "ExecStart=$out"
 
     cat >>$out/lib/systemd/user/warp-taskbar.service <<EOF
+
     [Service]
     BindReadOnlyPaths=$out:/usr:
     EOF
@@ -69,15 +53,19 @@ stdenv.mkDerivation rec {
   '';
 
   postInstall = ''
-    wrapProgram $out/bin/warp-svc --prefix PATH : ${lib.makeBinPath [ nftables ]}
+    wrapProgram $out/bin/warp-svc --prefix PATH : ${
+      lib.makeBinPath [ nftables ]
+    }
   '';
 
   meta = with lib; {
-    description = "Replaces the connection between your device and the Internet with a modern, optimized, protocol";
+    description =
+      "Replaces the connection between your device and the Internet with a modern, optimized, protocol";
     homepage = "https://pkg.cloudflareclient.com/packages/cloudflare-warp";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
-    maintainers = with maintainers; [ wolfangaukang ];
-    platforms = [ "x86_64-linux" ];
+    mainProgram = "warp-cli";
+    maintainers = with maintainers; [ devpikachu marcusramberg ];
+    platforms = [ "x86_64-linux" "aarch64-linux" ];
   };
 }
