635a449d1d38631f98c2c4bf808d89f8ef9e2255
Saksham Mittal <git@gotlou.com>
2025-06-16 22:51:48 +0530

    zed: use claus for Sonnet access

--- home/zed.nix
+++ home/zed.nix
@@ -25,7 +25,6 @@ in {
       gutter.runnables = false;
       gutter.line_numbers = true;
 
-      assistant.button = false;
       collaboration_panel.button = false;
       notification_panel.button = false;
       project_panel.button = false;
@@ -36,6 +35,69 @@ in {
       features.inline_completion_provider = "none";
       use_system_path_prompts = false;
       slash_commands.docs.enabled = true;
+
+      assistant.button = true;
+      assistant.enable_experimental_live_diffs = true;
+      assistant = {
+        version = "2";
+        default_model = {
+          model = "claude-opus-4-20250514";
+          provider = "anthropic";
+        };
+        show_hints = false;
+      };
+
+      language_models.anthropic.version = "1";
+      language_models.anthropic.api_url = "http://127.0.0.1:25784";
+      language_models.anthropic.available_models = let
+        ultra_think = {
+          max_output_tokens = 32000;
+          cache_configuration = {
+            min_total_token = 2048;
+            should_speculate = true;
+            max_cache_anchors = 4;
+          };
+          supports_tools = true;
+          supports_thinking = true;
+          max_tokens = 200000;
+          mode = {
+            type = "thinking";
+            budget_tokens = 32000;
+          };
+        };
+        normal_model = {
+          max_output_tokens = 32000;
+          cache_configuration = {
+            min_total_token = 2048;
+            should_speculate = true;
+            max_cache_anchors = 4;
+          };
+          max_tokens = 200000;
+          supports_thinking = false;
+        };
+      in [
+        # NOTE: this is for Max plans only
+        # (ultra_think // {
+        #   name = "claude-opus-4-20250514";
+        #   display_name = "Magnus";
+        # })
+        (ultra_think // {
+          name = "claude-sonnet-4-20250514";
+          display_name = "Claude Sonnet 4 (Thinking)";
+        })
+       (normal_model // {
+          name = "claude-sonnet-4-20250514";
+          display_name = "Claude Sonnet 4 (Normal)";
+        })
+       (normal_model // {
+          name = "claude-3-7-sonnet-20250219";
+          display_name = "Claude Sonnet 3.7 (Normal)";
+        })
+       (normal_model // {
+          name = "claude-3-5-haiku-20241022";
+          display_name = "Claude Haiku 3.5 (Normal)";
+        })
+      ];
     };
   };
 }
