d022efb7c319f02d63e1c925de6eea146097c4df
Saksham Mittal <git@gotlou.com>
2025-12-28 20:07:29 +0530

    tokidoki: fix hashes

--- overlays/tokidoki/tokidoki.nix
+++ overlays/tokidoki/tokidoki.nix
@@ -11,7 +11,7 @@ buildGoModule {
     sha256 = "sha256-3A4kyB2o+PdRcJYtU5k3miE45sEpp8XXwfYfmr+CA+c=";
   };
 
-  vendorHash = "sha256-pODLTMRHmMN7JYS8dbRLV8ltQnNrtKn88tKdRwxjH2o=";
+  vendorHash = "sha256-/Oj2wKqvaogS8cwXU+gY8eRwl6GHZIM6eLyy7fA0ZEg=";
 
   subPackages = [ "cmd/tokidoki" ];
 
