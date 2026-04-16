203d39a0787c6603ab57190c64dc7b755c1ca415
Saksham Mittal <git@gotlou.com>
2026-04-16 10:50:18 +0530

    openssh: harden sandbox

--- system/openssh.nix
+++ system/openssh.nix
@@ -2,6 +2,7 @@
 {
   services.openssh = {
     enable = true;
+    startWhenNeeded = true;
     settings = {
       PasswordAuthentication = true;
       KbdInteractiveAuthentication = false;
@@ -11,23 +12,42 @@
     };
   };
   networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 22 ];
-  systemd.services.sshd.serviceConfig = {
+  systemd.services."sshd@".serviceConfig = {
     ProtectSystem = true;
     PrivateTmp = true;
     ProtectClock = true;
     ProtectProc = "invisible";
+    ProcSubset = "pid";
     PrivateDevices = true;
     LockPersonality = true;
+    ProtectHostname = true;
     ProtectKernelTunables = true;
     ProtectKernelModules = true;
     ProtectKernelLogs = true;
     ProtectControlGroups = true;
     MemoryDenyWriteExecute = true;
-    RestrictSUIDGID = true;
+    RestrictSUIDSGID = true;
     RestrictRealtime = true;
+    RestrictNamespaces = true;
+    RestrictAddressFamilies = [
+      "AF_UNIX"
+      "AF_INET"
+      "AF_INET6"
+    ];
     # Protect Impermanence paths, which are full of private data
     InaccessiblePaths = [ "/persist" ];
     SystemCallArchitectures = "native";
     NoNewPrivileges = true;
+    UMask = "0077";
+    SystemCallFilter = [
+      "~@clock"
+      "~@cpu-emulation"
+      "~@debug"
+      "~@module"
+      "~@obsolete"
+      "~@raw-io"
+      "~@reboot"
+      "~@swap"
+    ];
   };
 }
