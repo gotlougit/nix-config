23a426e84b327b675a6fd76dfd1bff6603cc1cfd
Saksham Mittal <git@gotlou.com>
2026-04-16 10:30:51 +0530

    openssh: fix sandbox + only ssh over tailscale

--- system/openssh.nix
+++ system/openssh.nix
@@ -1,31 +1,33 @@
 { ... }:
 {
-  # Enable the OpenSSH daemon to act as SSH server
-  services.openssh.enable = true;
-  systemd.services.sshd = {
+  services.openssh = {
     enable = true;
-    serviceConfig = {
-      ProtectSystem = true;
-      PrivateTmp = true;
-      ProtectClock = true;
-      ProtectProc = "invisible";
-      PrivateDevices = true;
-      LockPersonality = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectKernelLogs = true;
-      ProtectControlGroups = true;
-      MemoryDenyWriteExecute = true;
-      RestrictSUIDGID = true;
-      RestrictRealtime = true;
-      # Protect Impermanence paths, which are full of private data
-      InaccessiblePaths = [ "/persist" ];
-      SystemCallArchitectures = "native";
-      NoNewPrivileges = true;
-      SystemCallFilter = [
-        "@system-service"
-        "~@resources"
-      ];
+    settings = {
+      PasswordAuthentication = true;
+      KbdInteractiveAuthentication = false;
+      PubkeyAuthentication = true;
+      PermitRootLogin = "no";
+      MaxAuthTries = 10;
     };
+  };
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 22 ];
+  systemd.services.sshd.serviceConfig = {
+    ProtectSystem = true;
+    PrivateTmp = true;
+    ProtectClock = true;
+    ProtectProc = "invisible";
+    PrivateDevices = true;
+    LockPersonality = true;
+    ProtectKernelTunables = true;
+    ProtectKernelModules = true;
+    ProtectKernelLogs = true;
+    ProtectControlGroups = true;
+    MemoryDenyWriteExecute = true;
+    RestrictSUIDGID = true;
+    RestrictRealtime = true;
+    # Protect Impermanence paths, which are full of private data
+    InaccessiblePaths = [ "/persist" ];
+    SystemCallArchitectures = "native";
+    NoNewPrivileges = true;
   };
 }
