21ab9e8196ce02ee5da321e6445f857061777ffb
Saksham Mittal <git@gotlou.com>
2026-06-06 17:01:52 +0530

    litert-lm: update

--- overlays/litert-lm/default.nix
+++ overlays/litert-lm/default.nix
@@ -1,11 +1,6 @@
 # Overlay to add litert-lm package to nixpkgs
 final: prev:
 
-let
-  litert-lm-api = prev.callPackage ./litert-lm-api.nix { };
-in
 {
-  litert-lm = prev.callPackage ./litert-lm.nix {
-    inherit litert-lm-api;
-  };
+  litert-lm = prev.callPackage ./litert-lm.nix { };
 }
