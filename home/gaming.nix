1563a3cd9df41394dce57770eada8c0d0fa206de
Saksham Mittal <git@gotlou.com>
2026-06-17 00:33:43 +0530

    rpcs3: rm

--- home/gaming.nix
+++ home/gaming.nix
@@ -8,7 +8,7 @@
     dolphin-emu # GameCube and Wii emulator
     # duckstation # PS1 emulator
     pcsx2 # PS2 emulator
-    rpcs3 # PS3 emulator
+    # rpcs3 # PS3 emulator
     # xemu # Original Xbox emulator
     luanti # Open source Minecraft-like game
     heroic # GOG + Epic + Amazon Game launcher + Proton wrapper to boot
