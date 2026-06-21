f30aa4edcc00b957d7aafeb927b7f6e06ae2ad89
Saksham Mittal <git@gotlou.com>
2026-06-21 23:01:49 +0530

    wireplumber: add support for better codecs etc

--- hosts/kratos/services/sound.nix
+++ hosts/kratos/services/sound.nix
@@ -13,8 +13,12 @@
 
   # Some WirePlumber stuff to handle bluetooth correctly
   services.pipewire.wireplumber.enable = true;
-  # environment.etc = {
-  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text =
-  #     "	bluez_monitor.properties = {\n		[\"bluez5.enable-sbc-xq\"] = true,\n		[\"bluez5.enable-msbc\"] = true,\n		[\"bluez5.enable-hw-volume\"] = true,\n		[\"bluez5.headset-roles\"] = \"[ hsp_hs hsp_ag hfp_hf hfp_ag ]\"\n	}\n";
-  # };
+  environment.etc."wireplumber/wireplumber.conf.d/50-bluetooth-config.conf".text = ''
+    monitor.bluez.properties = {
+      bluez5.enable-sbc-xq = true
+      bluez5.enable-msbc = true
+      bluez5.enable-hw-volume = true
+      bluez5.headset-roles = [ hsp_hs hsp_ag hfp_hf hfp_ag ]
+    }
+  '';
 }
