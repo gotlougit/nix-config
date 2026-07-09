af1676182a56c93a75793c7face32433ba9c703e
Saksham Mittal <git@gotlou.com>
2026-07-09 20:29:03 +0530

    gaming: reduce packages

--- home/gaming.nix
+++ home/gaming.nix
@@ -2,17 +2,24 @@
 
 {
   home.packages = with pkgs; [
-    # Gaming stuff
+
+    # Utils
+    chiaki-ng # PS4/5 Remote Play client
+    mangohud # Overlay for FPS, frametime, CPU, GPU usage etc
+
+    # Games
+    # luanti # Open source Minecraft-like game
+
+    # Launchers
+    # heroic # GOG + Epic + Amazon Game launcher + Proton wrapper to boot
+
+    # Emulators
     # cemu # Wii U emulator
-    # chiaki-ng # PS4/5 Remote Play client
-    dolphin-emu # GameCube and Wii emulator
+    # dolphin-emu # GameCube and Wii emulator
     # duckstation # PS1 emulator
-    pcsx2 # PS2 emulator
+    # pcsx2 # PS2 emulator
     # rpcs3 # PS3 emulator
     # xemu # Original Xbox emulator
-    luanti # Open source Minecraft-like game
-    heroic # GOG + Epic + Amazon Game launcher + Proton wrapper to boot
-    mangohud # Overlay for FPS, frametime, CPU, GPU usage etc
-    ryubing # Switch emulator
+    # ryubing # Switch emulator
   ];
 }
