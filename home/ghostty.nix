97c70df6da95dd73820e3f9b335b6aff85fef72b
Saksham Mittal <git@gotlou.com>
2026-07-09 11:32:56 +0530

    ghostty: fix new window pwd

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -10,17 +10,18 @@
 
       font-size = "12";
 
-      # Middle click pastes from the primary selection
-      # mouse-bind = "middle=primary_paste";
+      # Make new windows start at ~ by default
+      working-directory = "home";
+      window-inherit-working-directory = false;
 
       keybind = [
         # Paste from the clipboard
         "ctrl+shift+v=paste_from_clipboard"
 
-        # Split vertical (physical key: 9 with shift = "(" on US layout)
+        # Split vertical
         "ctrl+shift+9=new_split:right"
 
-        # Split horizontal (physical key: 0 with shift = ")" on US layout)
+        # Split horizontal
         "ctrl+shift+0=new_split:down"
 
         # Cycle through panes
