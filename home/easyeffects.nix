2e3b91b0f59055b435b1a3a86df417433daec238
Saksham Mittal <git@gotlou.com>
2026-09-22 21:43:49 +0530

    bluetooth: own btusb in userspace for handsfree

--- home/easyeffects.nix
+++ home/easyeffects.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  services.easyeffects.enable = true;
+  services.easyeffects.enable = false;
   services.easyeffects.preset = "thinkpad-unsuck";
   xdg.dataFile."easyeffects/output/thinkpad-unsuck.json" = {
     source = ./thinkpad-unsuck.json;
--- system/bluetooth.nix
+++ system/bluetooth.nix
@@ -2,5 +2,6 @@
 {
   hardware.bluetooth.enable = true;
   hardware.bluetooth.floss.users = [ "gotlou" ];
+  hardware.bluetooth.floss.usbTransport.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 }
